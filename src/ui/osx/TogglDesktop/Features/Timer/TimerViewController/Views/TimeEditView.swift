26498095b383388a18a8e7beac45ee19abcb93d7
Andrii Nester <nester.andrej@gmail.com>
2020-11-04 19:18:43 +0200

    Implement date control on duration dropdown (mac)

--- src/ui/osx/TogglDesktop/Features/Timer/TimerViewController/Views/TimeEditView.swift
+++ src/ui/osx/TogglDesktop/Features/Timer/TimerViewController/Views/TimeEditView.swift
@@ -11,6 +11,7 @@ import Cocoa
 class TimeEditView: NSView {
 
     var onStartTextChange: ((String) -> Void)?
+    var onStartDateChange: ((Date) -> Void)?
 
     var startStringValue: String {
         get {
@@ -22,19 +23,126 @@ class TimeEditView: NSView {
         }
     }
 
+    var startDateValue: Date {
+        get {
+            dateValue
+        }
+        set {
+            update(with: newValue)
+        }
+    }
+
+    private var dateValue: Date = Date() {
+        didSet {
+            if dateValue > Date() {
+                dateValue = Date()
+            }
+            update(with: dateValue)
+            onStartDateChange?(dateValue)
+        }
+    }
+
     private var isStartFieldChanged = false
 
+    private var isCalendarVisible: Bool {
+        calendarView.superview != nil
+    }
+
+    private enum Constants {
+        static let dayNameAttribute: [NSAttributedString.Key: Any] = {
+            return [NSAttributedString.Key.font: NSFont.systemFont(ofSize: 14),
+                    NSAttributedString.Key.foregroundColor: NSColor.togglBlackText]
+        }()
+
+        static let calendarTopPadding: CGFloat = 8
+    }
+
+    // MARK: - Views
+
+    @IBOutlet private weak var contentView: NSView!
     @IBOutlet private weak var startTextField: NSTextField!
+    @IBOutlet private weak var todayButton: CursorButton!
+    @IBOutlet private weak var datePicker: KeyboardDatePicker!
+    @IBOutlet private weak var dateBox: NSBox!
+
+    private var calendarView: NSView { calendarViewControler.view }
+
+    private lazy var calendarViewControler: CalendarViewController = {
+        let controller = CalendarViewController(nibName: NSNib.Name("CalendarViewController"), bundle: nil)
+        controller.delegate = self
+        return controller
+    }()
+
+    // MARK: - Overrides
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        dateValue = Date()
+    }
 
     override func becomeFirstResponder() -> Bool {
         startTextField.becomeFirstResponder()
     }
+
+    // MARK: - Actions
+
+    @IBAction func previousDayButtonClicked(_ sender: Any) {
+        dateValue = dateValue.previousDate() ?? dateValue
+    }
+
+    @IBAction func nextDayButtonClicked(_ sender: Any) {
+        dateValue = dateValue.nextDate() ?? dateValue
+    }
+
+    @IBAction func datePickerChanged(_ sender: Any) {
+        dateValue = datePicker.dateValue
+    }
+
+    @IBAction func todayButtonClicked(_ sender: Any) {
+        isCalendarVisible ? hideCalendar() : showCalendar()
+    }
+
+    // MARK: - Private
+
+    private func update(with date: Date) {
+        todayButton.attributedTitle = NSAttributedString(
+            string: date.dayOfWeekString() ?? "Unknown",
+            attributes: Constants.dayNameAttribute
+        )
+        calendarViewControler.prepareLayout(with: date)
+        datePicker.dateValue = date
+        datePicker.maxDate = Date()
+    }
+
+    private func showCalendar() {
+        guard calendarView.superview == nil else { return }
+
+        calendarView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(calendarView)
+
+        NSLayoutConstraint.activate([
+            calendarView.topAnchor.constraint(equalTo: dateBox.bottomAnchor, constant: Constants.calendarTopPadding),
+            calendarView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            calendarView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            calendarView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
+        ])
+
+        window?.setContentSize(frame.size)
+    }
+
+    private func hideCalendar() {
+        calendarView.removeFromSuperview()
+        window?.setContentSize(frame.size)
+    }
 }
+
+// MARK: - NSTextFieldDelegate
 
 extension TimeEditView: NSTextFieldDelegate {
     func controlTextDidEndEditing(_ obj: Notification) {
         if obj.object as? NSTextField == startTextField, isStartFieldChanged {
             onStartTextChange?(startStringValue)
+            isStartFieldChanged = false
         }
     }
 
@@ -50,5 +158,18 @@ extension TimeEditView: NSTextFieldDelegate {
                 window?.orderOut(nil)
         }
         return false
+    }
+}
+
+// MARK: - CalendarViewControllerDelegate
+
+extension TimeEditView: CalendarViewControllerDelegate {
+
+    func calendarViewControllerDidSelect(date: Date) {
+        dateValue = date
+    }
+
+    func calendarViewControllerDoneBtnOnTap() {
+        hideCalendar()
     }
 }
